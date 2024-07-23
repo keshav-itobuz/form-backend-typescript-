5dd6c7a40302b16d82bff04744453229fbf3a826
Keshav Kumar <keshavkumar@Keshavs-MacBook-Pro.local>
2024-07-23 13:42:37 +0530

    updated

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -6,68 +6,81 @@ import mongoose from 'mongoose'
 import employeeSchema from '../validator/employeeValidation'
 
 class employeeClass {
+    public getEmployee: RequestHandler = async (req, res, next) => {
+        try {
+            const { profession, page, limit, searchedName } = req.query
 
-  public getEmployee: RequestHandler = async (req, res, next) => {
-    try {
-      const { profession, page, limit } = req.query
-      const employeeProfession =
-        profession === Profession.ALL ? {} : { profession }
-      const employeeData = await Employee.find(employeeProfession)
-        .skip(10 * Number(page))
-        .limit(Number(limit))
-      let total = await Employee.countDocuments(employeeProfession)
-      res.status(StatusCode.ACCEPTED).json({
-        data: { employeeData, total },
-        message: 'Data received successfully',
-        success: true,
-      })
-    } catch (error) {
-      next(error)
+            const employeeProfession =
+                profession === Profession.ALL ? {} : { profession }
+
+            const employeeName = searchedName
+                ? { name: { $regex: searchedName, $options: 'i' } }
+                : {}
+
+            const employeeData = await Employee.find({
+                ...employeeProfession,
+                ...employeeName,
+            })
+                .skip(10 * Number(page))
+                .limit(Number(limit))
+            let total = await Employee.countDocuments({
+                ...employeeProfession,
+                ...employeeName,
+            })
+
+            res.status(StatusCode.ACCEPTED).json({
+                data: { employeeData, total },
+                message: 'Data received successfully',
+                success: true,
+            })
+        } catch (error) {
+            next(error)
+        }
     }
-  }
 
-
-  public deleteEmployee: RequestHandler = async (req, res, next) => {
-    try {
-      const _id = req.query.id ? { _id: req.query.id } : {};
-      const deletedData = await Employee.deleteMany(_id)
-      res.status(StatusCode.NO_CONTENT).json({
-        data: null,
-        message: 'Successfully deleted all the data',
-        success: true,
-      })
-    } catch (error) {
-      next(error)
+    public deleteEmployee: RequestHandler = async (req, res, next) => {
+        try {
+            const _id = req.query.id ? { _id: req.query.id } : {}
+            const deletedData = await Employee.deleteMany(_id)
+            res.status(StatusCode.NO_CONTENT).json({
+                data: null,
+                message: 'Successfully deleted all the data',
+                success: true,
+            })
+        } catch (error) {
+            next(error)
+        }
     }
-  }
 
-  public createUpdateEmployee: RequestHandler = async (req, res, next) => {
-    try {
-      const validate = employeeSchema.safeParse(req.body.employeeData)
-      if (!validate.success) {
-        throw new Error(validate?.error?.errors[0]?.message)
-      }
-      const { email, _id } = req.body.employeeData
-      const duplicateEntry = await Employee.findOne({ email })
-      if (duplicateEntry && !duplicateEntry._id.equals(_id)) {
-        throw new Error('Duplicate email')
-      }
-      req.body.employeeData._id = _id ? _id : new mongoose.Types.ObjectId()
-      const updatedData = await Employee.findByIdAndUpdate(
-        req.body.employeeData._id,
-        req.body.employeeData,
-        { upsert: true, returnOriginal: false }
-      )
-      res.status(StatusCode.OK).json({
-        data: null,
-        message: updatedData
-          ? 'Data updated successfully'
-          : 'Data uploaded successfully',
-        success: true,
-      })
-    } catch (error) {
-      next(error)
+    public createUpdateEmployee: RequestHandler = async (req, res, next) => {
+        try {
+            const validate = employeeSchema.safeParse(req.body.employeeData)
+            if (!validate.success) {
+                throw new Error(validate?.error?.errors[0]?.message)
+            }
+            const { email, _id } = req.body.employeeData
+            const duplicateEntry = await Employee.findOne({ email })
+            if (duplicateEntry && !duplicateEntry._id.equals(_id)) {
+                throw new Error('Duplicate email')
+            }
+            req.body.employeeData._id = _id
+                ? _id
+                : new mongoose.Types.ObjectId()
+            const updatedData = await Employee.findByIdAndUpdate(
+                req.body.employeeData._id,
+                req.body.employeeData,
+                { upsert: true, returnOriginal: false }
+            )
+            res.status(StatusCode.OK).json({
+                data: null,
+                message: updatedData
+                    ? 'Data updated successfully'
+                    : 'Data uploaded successfully',
+                success: true,
+            })
+        } catch (error) {
+            next(error)
+        }
     }
-  }
 }
 export const employeeOperations = new employeeClass()
--- src/routes/employeeRoutes.ts
+++ src/routes/employeeRoutes.ts
@@ -6,9 +6,8 @@ const router = Router()
 
 router.get('/get-employee', employeeOperations.getEmployee)
 
-router.post('/create-update-employee',  employeeOperations.createUpdateEmployee)
+router.post('/create-update-employee', employeeOperations.createUpdateEmployee)
 
 router.delete('/delete-employee', employeeOperations.deleteEmployee)
-
 
 export default router
